66f2478483d1c1c66f378bc1736273124e433b23
Manav Dawar <manavdawar5@gmail.com>
2018-07-10 23:53:22 +0530

    first commit

---
+++ app/src/main/java/com/example/manav/log/SecondActivity.java
@@ -0,0 +1,66 @@
+package com.example.manav.log;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        webView = (WebView)findViewById(R.id.wv);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://passport.gov.in/oci/welcome");
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+
+    public void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this,MainActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
